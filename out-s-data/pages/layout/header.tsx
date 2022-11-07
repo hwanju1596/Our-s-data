b187715e186e9cd4ef2e32f53f9258b4fa4f5eb7
hwanju <hwanju1596@gmail.com>
2022-11-07 00:20:51 +0000

    [header-ui] 1. 툴팁제거 2. Prettier

--- out-s-data/pages/layout/header.tsx
+++ out-s-data/pages/layout/header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
-import styled from "@emotion/styled";
-import { Button, Stack, IconButton, Popper, Fade, Box, Badge, Tooltip } from "@mui/material";
+import {
+  Button,
+  Stack,
+  IconButton,
+  Popper,
+  Fade,
+  Box,
+  Badge,
+} from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
@@ -19,50 +26,38 @@ const SearchButton = () => {
 const NotificationsButton = () => {
   const [open, setOpen] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  // const [tooltipOpen, setTooltipOpen] = React.useState(false);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
     setOpen((previousOpen) => !previousOpen);
-    // setTooltipOpen(open);
   };
 
   const canBeOpen = open && Boolean(anchorEl);
-  const id = canBeOpen ? 'transition-popper' : undefined;
- 
+  const id = canBeOpen ? "transition-popper" : undefined;
+
   return (
     <>
-    <Tooltip title="공지" >
-      <IconButton
-        size="large"
-        color="primary"
-        onClick={handleClick}
-      >
+      <IconButton size="large" color="primary" onClick={handleClick}>
         <Badge badgeContent={4} color="error">
           <NotificationsIcon />
         </Badge>
       </IconButton>
-    </Tooltip>
-      <Popper id={id} open={open} anchorEl={anchorEl} placement="bottom-end" transition> 
-      {({ TransitionProps }) => (
+      <Popper
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        placement="bottom-end"
+        transition
+      >
+        {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
-            <Box sx={{ border: 1, p: 1, bgcolor: 'primary' }}>
+            <Box sx={{ border: 1, p: 1, bgcolor: "primary" }}>
               The content of the Popper.
             </Box>
           </Fade>
         )}
       </Popper>
     </>
-  );
-};
-
-const ToolbarUI = () => {
-
-  return (
-    <Toolbar variant=	'string'>
-      <SearchButton />
-      <NotificationsButton/>
-    </Toolbar>
   );
 };
 
@@ -75,7 +70,8 @@ const Header = () => {
         alignItems="center"
         spacing={2}
       >
-        <ToolbarUI />
+      <SearchButton />
+      <NotificationsButton />
       </Stack>
     </div>
   );
